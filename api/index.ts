83d96d1427df827098c93fe2995611f494b6e8dd
T-Philipp <philipp.tavra@lusini.com>
2025-01-18 11:58:28 +0100

    update express server

--- api/index.ts
+++ api/index.ts
@@ -1,58 +1,63 @@
-import express from 'express'
+import express, { Response } from 'express'
 import path from 'path'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Get the current UTC date
-const utcDate = new Date()
+const utcDate: Date = new Date()
 
 // Convert UTC date to Berlin time
-const deDateTime = utcDate.toLocaleString('de-DE', {
+const deDateTime: string = utcDate.toLocaleString('de-DE', {
   timeZone: 'Europe/Berlin'
 })
-const deDate = utcDate.toLocaleString('de-DE', {
+const deDate: string = utcDate.toLocaleString('de-DE', {
   timeZone: 'Europe/Berlin',
   day: '2-digit',
   month: 'short',
   year: 'numeric'
 })
-const deDay = utcDate.toLocaleString('de-DE', {
+const deDay: string = utcDate.toLocaleString('de-DE', {
   timeZone: 'Europe/Berlin',
   weekday: 'short'
 })
-const deTime = utcDate.toLocaleString('de-DE', {
+const deTime: string = utcDate.toLocaleString('de-DE', {
   timeZone: 'Europe/Berlin',
   timeStyle: 'medium'
 })
 
 // Create output object
-const zeiten_output = {
+const zeiten_output: {
+  dateTime: string
+  date: string
+  day: string
+  time: string
+} = {
   dateTime: deDateTime,
   date: deDate,
   day: deDay,
   time: deTime
 }
 
-app.use(express.static('public'))
-
 // Serve the main HTML file
-app.get('/', function (_, res) {
+app.get('/', function (_, res: Response): void {
   res.sendFile(path.join(__dirname, '..', 'sites', 'index.html'))
 })
 
 // Serve the JSON file with additional times
-app.get('/zus-zeiten.json', function (_, res) {
+app.get('/zus-zeiten.json', function (_, res: Response): void {
   res.setHeader('Content-Type', 'application/json')
   res.sendFile(path.join(__dirname, '..', 'sites', 'zus-zeiten.json'))
 })
 
 // Serve the current time in Berlin
-app.get('/time', function (_, res) {
+app.get('/time', function (_, res: Response): void {
   res.setHeader('Content-Type', 'application/json')
   res.send(JSON.stringify(zeiten_output))
 })
 
 // Start the server
-app.listen(3000, () => console.log('Server ready on port 3000.'))
+// app.listen(4000, (): void => console.log('Server ready on port 3000.'))
 
-module.exports = app
+export default app
